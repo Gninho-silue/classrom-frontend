0f761b30fb20e60fc1f54b0d0c70f1e61a3de077
Silue <gninhosilue@gmail.com>
2026-03-02 23:29:47 +0000

    finalize create class form

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -1,7 +1,7 @@
 import { CreateView } from "@/components/refine-ui/views/create-view.tsx";
 import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { useBack } from "@refinedev/core";
+import { useBack, useList } from "@refinedev/core";
 import { Separator } from "@/components/ui/separator.tsx";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -36,7 +36,7 @@ const Create = () => {
 
     });
 
-    const {
+    const { refineCore: { onFinish},
         handleSubmit,
         formState: { isSubmitting, errors },
         control,
@@ -44,35 +44,38 @@ const Create = () => {
 
     const onSubmit = async (values: z.infer<typeof classSchema>) => {
         try {
-            console.log(values);
+            await onFinish(values);
         } catch (error) {
             console.error("Error creating class:", error);
         }
     };
 
-    const teachers = [
-        {
-            id: 1,
-            name: "John Doe",
+    const { query: subjectsQuery } = useList({
+        resource: "subjects",
+        pagination: {
+            pageSize: 100,
         },
-        {
-            id: 2,
-            name: "Jane Doe",
-        },
-    ];
+    });
 
-    const subjects = [
-        {
-            id: 1,
-            name: "Math",
-            code: "MATH",
+    const { query: teachersQuery } = useList({
+        resource: "users",
+        filters: [
+            {
+                field: "role",
+                operator: "eq",
+                value: "teacher",
+            },
+        ],
+        pagination: {
+            pageSize: 100,
         },
-        {
-            id: 2,
-            name: "English",
-            code: "ENG",
-        },
-    ];
+    });
+
+    const subjects = subjectsQuery?.data?.data || [];
+    const subjectsLoading = subjectsQuery?.isLoading;
+
+    const teachers = teachersQuery?.data?.data || [];
+    const teachersLoading = teachersQuery?.isLoading;
 
     const bannerPublicId = form.watch("bannerCldPubId");
     const setBannerImage = (file: any, field: any) => {
@@ -125,14 +128,15 @@ const Create = () => {
                                             <FormLabel>
                                                 Banner Image <span className="text-orange-600">*</span>
                                             </FormLabel>
+                                            <FormDescription>
+                                                Upload a banner image for the class.
+                                            </FormDescription>
                                             <FormControl>
                                                 <UploadWidget value={field.value ? { url: field.value, publicId: bannerPublicId ?? '' } : null}
                                                     onChange={(file: any) => setBannerImage(file, field)} />
                                             </FormControl>
                                             <FormMessage />
-                                            <FormDescription>
-                                                Upload a banner image for the class.
-                                            </FormDescription>
+
                                             {errors.bannerCldPubId && !errors.bannerUrl && (
                                                 <p className="text-destructive">{errors.bannerCldPubId.message?.toString()}</p>
                                             )}
@@ -173,6 +177,7 @@ const Create = () => {
                                                         field.onChange(Number(value))
                                                     }
                                                     value={field.value?.toString()}
+                                                    disabled={subjectsLoading}
                                                 >
                                                     <FormControl>
                                                         <SelectTrigger className="w-full">
@@ -206,6 +211,7 @@ const Create = () => {
                                                 <Select
                                                     onValueChange={field.onChange}
                                                     value={field.value}
+                                                    disabled={teachersLoading}
                                                 >
                                                     <FormControl>
                                                         <SelectTrigger className="w-full">
